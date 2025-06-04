585254e47944557eb844d29320c60e682dbd0886
obediobadiah <obediobadiah05@gmail.com>
2025-06-05 00:02:44 +0200

    [ Vercel Json config ]

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// This file is the entry point for Vercel serverless function
-const app = require('../dist/index.js');
-
-// Export the Express app as a serverless function
-module.exports = app; 
